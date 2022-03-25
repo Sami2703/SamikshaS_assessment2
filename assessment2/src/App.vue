712ef70b51f4aaf0f955affe891b902d2431819e
Sami_2703 <samikshasapat03@gmail.com>
2022-03-25 12:05:00 +0530

    https://github.com/Sami2703/SamikshaS_assessment2.git

---
+++ assessment2/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <header-bar></header-bar>
+    <b-container>
+      <b-row class="mt-5">
+        <router-view />
+      </b-row>
+    </b-container>
+    <DataTable/>
+    <!-- <customer-overview/>
+    <CreateCustomerForm/>
+    
+    <EditCustomerForm/>
+    <HomeCustomer/> -->
+  </div>
+</template>
+
+<script>
+import HeaderBar from "@/components/HeaderBar.vue";
+// import CustomerOverview from "@/components/CustomerOverview.vue";
+// import CreateCustomerForm from "@/components/CreateCustomerForm.vue";
+import DataTable from "@/components/DataTable.vue";
+// import EditCustomerForm from "@/components/EditCustomerForm.vue";
+// import HomeCustomer from "@/components/HomeCustomer.vue";
+
+
+
+export default {
+  components: {
+    HeaderBar,
+    DataTable
+
+  },
+};
+</script>
+
